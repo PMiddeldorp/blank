c3f7f0dbcabbc4464fc4265ba7b6b3a03977675b
Pieter Middeldorp <pieter@buromiddeldorp.nl>
2022-09-29 15:31:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php get_header(); ?>
 
-<main role="main">
-	<!-- section -->
-	<section>
+<main id="main">
+	<div class="container">
 
 		<h1>Latest posts</h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
+		<article>
+			<header>
+				<h2><?php the_title();?></h2>
+				<?php the_excerpt();?>
+				<a href="<?php the_permalink();?>">read more</a>
+			</article>
+		</article>
 		<?php endwhile; ?>
 		<?php else: ?>
 		<?php endif; ?>
 
-	</section>
-	<!-- /section -->
+
+	</div>
 </main>
 
 <?php get_footer(); ?>
